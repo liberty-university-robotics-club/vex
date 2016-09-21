3415db932578809cd50f076961f0f9f1fdfb2b84
root <root@DESKTOP-181F2NV.localdomain>
2016-09-20 20:00:32 -0800

    added generic control loop

---
+++ src/controlloop.c
@@ -0,0 +1,13 @@
+#include "../include/controlloop.h"
+
+int controlLoop(int current, int target, state* state)
+{
+    int increase = 1;
+    if (current < target)
+    {
+        return current += increase;
+    }else
+    {
+        return current -= increase;
+    }
+}
