6a735caf4bdfff7608ed7927b958fd0162f43089
Elijah Barker <ebarker3@liberty.edu>
2017-02-07 20:25:23 -0500

    making driveoperation more auto programable

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -22,7 +22,7 @@ void controlmotors(int lb, int lf, int rb, int rf)
 void controldrive(int t, int f, int s)
 {
 	static int delprint=0;
-	(delprint++)%10 ? printf("controll!\n"):delprint;
+	(delprint++)%10 ? printf("controll!\r\n"):delprint;
 	static state *state_lb=NULL;
 	static state *state_lf=NULL;
 	static state *state_rb=NULL;
@@ -75,14 +75,28 @@ void controldrive(int t, int f, int s)
 	);
 }
 
-void driveoperation()
+void op_auto_drive(bool auto_mode)// add parameters
 {
 	int joyforwardT  = (abs(joystickGetAnalog(1,JOY_FORWARD_T )) > JOY_DEAD_T) ? joystickGetAnalog(1,JOY_FORWARD_T ) : 0;
 	int joyforwardS  = (abs(joystickGetAnalog(1,JOY_FORWARD_S )) > JOY_DEAD_T) ? joystickGetAnalog(1,JOY_FORWARD_S ) : 0;
 	int joyturnT     = (abs(joystickGetAnalog(1,JOY_TURN_T    )) > JOY_DEAD_T) ? joystickGetAnalog(1,JOY_TURN_T    ) : 0;
 	int joystrafingS = (abs(joystickGetAnalog(1,JOY_STRAFING_S)) > JOY_DEAD_T) ? joystickGetAnalog(1,JOY_STRAFING_S) : 0;
-
+	
+	if (!auto_mode)
+	{
+		
+	}
+	else
+	{
+		
+	}
+	
 	controldrive(joyturnT,joyforwardT+joyforwardS,joystrafingS);
+}
+
+void op_drive()
+{
+	op_auto_drive(false);// add arguments
 }
 
 bool isLiftUp()
@@ -95,7 +109,7 @@ bool isLiftDown()
 	return !digitalRead(BTN_DWN);//switch pressed is low => 0
 }
 
-int op_auto_lift(bool op_mode, bool begin_auto, bool go_up)
+int op_auto_lift(bool auto_mode, bool begin_auto, bool go_up)
 {
 	//first call:
 	static int lift_auto=0;
@@ -104,7 +118,7 @@ int op_auto_lift(bool op_mode, bool begin_auto, bool go_up)
 	
 	//every call:
 	lift_pow=0;
-	if(op_mode)
+	if(!auto_mode)//if in operator mode:
 	{
 		// get op button input
 		lift_pow += joystickGetDigital( 1 , JOY_LIFT_OP , JOY_UP   ) ? MLI_POW : 0 ;
@@ -170,9 +184,19 @@ void op_lift()
 	op_auto_lift(false, false, false);
 }
 
+//move to auto.c
+void drop_object()//assume lift is at top
+{
+	//go forward until hit;
+	start_auto_lift(false);
+	
+	
+}
+
+
 void opcontrol()
 {
-	driveoperation();
+	op_drive();
 	op_lift();
 }
 
