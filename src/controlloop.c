3efb1dd62636fce30c91bc46cfe4a365c3b697de
Linux User <swiley@thinkpad.my.domain>
2018-04-13 00:54:31 +0000

    notes from talk

--- src/controlloop.c
+++ src/controlloop.c
@@ -1,6 +1,12 @@
 #include "../include/controlloop.h"
 #include <math.h>
 
+//TODO: normalise T:
+// m ( e + (64/(t_j + e_j))t)
+//where j is max_i(|e_i|)
+//this will slow the robot down when theere is large error between motors so that they wont clip
+
+//TODO: somehwere else, scale p (current) so it's in same range as target
 int controlLoop(int current, int target, state* st)
 {
 	int dist=fabs(current-target);
